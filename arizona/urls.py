2184b0428a2b506e274d50654ad3875d5dcfcd6a
Ephraim Arizona <degreatarizona@gmail.com>
2025-01-03 21:56:00 +0100

    add backend

--- arizona/urls.py
+++ arizona/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('blog', views.blog, name='blog'),
+    path('contact', views.contact, name='contact'),
+    path('product', views.product, name='product'),
+    path('project', views.project, name='project'),
+    path('quote', views.quote, name='quote'),
+    path('service', views.service, name='service'),
+    path('training', views.training, name='training'),
 ]
--- arizona/views.py
+++ arizona/views.py
@@ -3,4 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    pass
+    return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+    
+def blog(request):
+    return render(request, 'blog.html', {})
+    
+def contact(request):
+    return render(request, 'contact.html', {})
+    
+def product(request):
+    return render(request, 'product.html', {})
+    
+def project(request):
+    return render(request, 'project.html', {})
+    
+def quote(request):
+    return render(request, 'quote.html', {})
+    
+def service(request):
+    return render(request, 'service.html', {})
+    
+def training(request):
+    return render(request, 'training.html', {})
+    
